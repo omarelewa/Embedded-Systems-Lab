093af8578823cd10324f49eb635b7cd4f4c48bf6
Omar Elewa <68481601+omarelewa@users.noreply.github.com>
2021-09-21 17:39:49 +0200

    Update main.c
    Corrected

--- main.c
+++ main.c
@@ -1,85 +1,89 @@
-/* This program blinks the red LED on the
-* TI Tiva LaunchPad. The connections are:
-* PF1 - red LED
-* PF2 - blue LED
-* PF3 - green LED
-* They are high active (a '1' turns on the LED).
-*/
 #include "TM4C123GH6PM.h"
 
+// Defining the bits responsible for LED color in GPIOF
 #define RED 0x2
 #define BLUE 0x4
 #define GREEN 0x8
 
+// 1 second
+#define TIME_PERIOD 1000
+#define INTERVAL 250
+#define TIME_DIVIDER 4
+
+// Old function for delay
 void delayMs(int n);
+
+// New improved and implemented function for delay
 void delay(int n);
+
+// Blink responsible for the blinking of LEDs every 500ms
+int blink(int toggle, int color);
 
 int main(void)
 {
-	int count = 0;
+	// LED
+	// 0 -> RED
+	// 1 -> BLUE
+	// 2 -> GREEN
+	int led = 0;
+	
+	// Used to change the state of LED without pausing the blinking process
 	int toggle = 0;
+	
+	// Counter used to check for a sustained change in the state of
+	// sw1 for more than 2 seconds
 	int change_couter_sw1 = 0;
+	
+	// Counter used to check for a sustained change in the state of
+	// sw2 for more than 2 seconds
 	int change_couter_sw2 = 0;
+	
+	// To record the state of sw1
 	unsigned int state_sw1;
+	
+	// To record the state of sw1
 	unsigned int state_sw2;
+	
+	// To check whether the blinking should be on or off
+	// 0 -> OFF
+	// 1 -> ON
 	int power = 0;
 	
+	// Enable clock to GPIOF
 	SYSCTL->RCGCGPIO |= 0x20;
+	
+	// Unlock GPIOCR register
 	GPIOF->LOCK = 0x4C4F434B;
+	
+	// Enable GPIOPUR register to commit
 	GPIOF->CR  = 0x01;
+	
+	// Enable Pull-up resistor for PF0 and PF4
 	GPIOF->PUR = 0x11;
+	
+	// Set PF1, PF2 and PF3 as output
+	// And set PF0 and PF4 as input
 	GPIOF->DIR = 0x0e;
+	
+	// Enable all pins as digital GPIO pins
 	GPIOF->DEN = 0x1f;
 	
 	while(1)
 	{
 		if (power%2 == 0)
 		{
-			if (count % 3 == 0)
+			if (led % 3 == 0)
 			{
-				if(toggle%4 == 0 || toggle%4 == 1)
-				{
-					GPIOF->DATA = RED;
-					delay(250);
-					toggle++;
-				}
-				if(toggle%4 == 2 || toggle%4 == 3)
-				{
-					GPIOF->DATA = 0; 
-					delay(250);
-					toggle++;
-				}
+				toggle = blink(toggle, RED);
 			}
 
-			else if (count % 3 == 1)
+			else if (led % 3 == 1)
 			{
-				if(toggle%4 == 0 || toggle%4 == 1)
-				{
-					GPIOF->DATA = BLUE;
-					delay(250);
-					toggle++;
-				}
-				if(toggle%4 == 2 || toggle%4 == 3)
-				{
-					GPIOF->DATA = 0; 
-					delay(250);
-					toggle++;
-				}
+				toggle = blink(toggle, BLUE);
 			}
 			else 
 			{
-				if(toggle%4 == 0 || toggle%4 == 1)
-				{
-					GPIOF->DATA = GREEN;
-					delay(250);
-					toggle++;
-				}
-				if(toggle%4 == 2 || toggle%4 == 3)
-				{
-					GPIOF->DATA = 0; 
-					delay(250);
-					toggle++;
-				}
+				toggle = blink(toggle, GREEN);
 			}
 		}
 		else
@@ -88,21 +92,26 @@ int main(void)
 		}
 			
 		state_sw1 = GPIOF->DATA & 0x01;
-		if (!state_sw1){	
-			
+		if (!state_sw1)
+		{		
 			change_couter_sw1++;
-			
-			if (change_couter_sw1%8 == 0){
-				count++;
+			if (change_couter_sw1%8 == 0)
+			{
+				led++;
+				toggle = 0;
 			}
+		}
+		else 
+		{
+			change_couter_sw1 = 0;
 		}
 		
 		state_sw2 = GPIOF->DATA & 0x10;
-		if (!state_sw2){	
-			
+		if (!state_sw2)
+		{	
 			change_couter_sw2++;
-			
-			if (change_couter_sw2%8 == 0){
+			if (change_couter_sw2%8 == 0)
+			{
 				power++;
 				if (power%2 == 1)
 				{
@@ -110,6 +119,10 @@ int main(void)
 					toggle = 0;
 				}
 			}
+		}
+		else 
+		{
+			change_couter_sw2 = 0;
 		}
 	}
 }
@@ -128,8 +141,28 @@ void delayMs(int n)
 void delay(int n)
 {
 	SysTick->LOAD = 15999*n;
-	SysTick->CTRL = 0x5; /*Enable the timer and choose sysclk */
-	while((SysTick->CTRL & 0x10000) == 0) /*wait until the Count flag is set */
+	/*Enable the timer and choose sysclk */
+	SysTick->CTRL = 0x5; 
+	/*wait until the Count flag is set */
+	while((SysTick->CTRL & 0x10000) == 0) 
 	{ }
-	SysTick->CTRL = 0; /*Stop the timer (Enable = 0) */
+	/*Stop the timer (Enable = 0) */
+	SysTick->CTRL = 0; 
+}
+
+int blink(int toggle, int color)
+{
+	if(toggle%4  == 0 || toggle%4 == 1)
+	{
+		GPIOF->DATA = color;
+		delay(INTERVAL);
+		toggle++;
+	}
+	else if(toggle%4 == 2 || toggle%4 == 3)
+	{
+		GPIOF->DATA = 0; 
+		delay(INTERVAL);
+		toggle++;
+	}
+	return toggle;
 }
